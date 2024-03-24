f54d8ee0574735848b5352a18c6d82736282d8e4
MehmetYukselSekeroglu <dijital_evren@protonmail.com>
2024-03-24 18:37:44 +0300

    ExampleProject_SystemInformationKit Added

---
+++ Cpp/ExampleProject_SystemInformatıonKit/include/system_info.h
@@ -0,0 +1,7 @@
+#ifndef SYSTEM_INFO_H
+#define SYSTEM_INFO_H
+
+void printSystemInformation(void);
+
+
+#endif
---
+++ Cpp/ExampleProject_SystemInformatıonKit/include/time_tools.h
@@ -0,0 +1,8 @@
+#ifndef TIME_TOOLS_H
+#define TIME_TOOLS_H
+
+#include <iostream>
+
+std::string getCurrentTime(void);
+
+#endif
