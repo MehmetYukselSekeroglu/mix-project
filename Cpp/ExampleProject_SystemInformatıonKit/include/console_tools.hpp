d155a8dbd8f5da9303599ef3dce919c9f24c3f50
MehmetYukselSekeroglu <dijital_evren@protonmail.com>
2024-04-15 18:34:48 +0300

    OpenCV test file added

--- Cpp/ExampleProject_SystemInformatıonKit/include/console_tools.hpp
+++ Cpp/ExampleProject_SystemInformatıonKit/include/console_tools.hpp
@@ -4,10 +4,10 @@
 #include <iostream>
 
 
-void p_info(const std::string& targetText);
-void p_warn(const std::string& targetText);
-void p_error(const std::string& targetText);
-void p_log(const std::string& targetText);
+void p_info(const std::string&);
+void p_warn(const std::string&);
+void p_error(const std::string&);
+void p_log(const std::string&);
 
 
 
--- Cpp/ExampleProject_SystemInformatıonKit/include/conversions.hpp
+++ Cpp/ExampleProject_SystemInformatıonKit/include/conversions.hpp
@@ -3,5 +3,5 @@
 #include <iostream>
 #include <stdint.h>
 // Example: void p_info(const std::string& targetText);
-double byte2gbyte(const uint64_t& byteSize);
+double byte2gbyte(const uint64_t&);
 #endif
